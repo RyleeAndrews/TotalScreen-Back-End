c2bec19fe568f1b6489643ff0459d615e0b25f51
Rylee Andrews <ryleeandrews73@gmail.com>
2018-08-30 13:32:23 -0600

    base

---
+++ routes/orderRoute.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Order = require('../models/orders.js');
+const bodyParser = require('../lib/middleware/bodyParser.js');
+const jsonParser = require('body-parser').json();
+const ordersRouter = module.exports = require('express').Router();
+
+ordersRouter.post('/postorder', bodyParser, ( req, res, next ) => {
+  try {
+    let order = new Order(req.body);
+
+    console.log('model', req.params);
+    order.save()
+      .then( result => res.status(200).send(result))
+      .catch(err => next(err));
+  }
+  catch(error){
+    next(error.message);
+  }
+});
